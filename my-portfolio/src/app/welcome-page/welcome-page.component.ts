69596e94fe4b1ac5207ce69454b4ae98b614d0de
MarcelZapadka <zapadka.m@wp.pl>
2022-10-04 17:19:46 +0200

    My portfolio
    Personal portfolio

--- my-portfolio/src/app/welcome-page/welcome-page.component.ts
+++ my-portfolio/src/app/welcome-page/welcome-page.component.ts
@@ -10,23 +10,33 @@ gsap.registerPlugin(TextPlugin);
 })
 
 export class WelcomePageComponent implements OnInit {
-
+  
   mainTimeLine = gsap.timeline();
   
   goToMainPage() {
-  this.mainTimeLine.to(".main-page-button", {rotate: 360, scaleX: 0, scaleY: 0, duration: 1, })
-  .to(".text", {opacity: 0, duration: 1})
-  .to(".starter-screen", {duration:1, scaleX: 0, scaleY: 0,})
+    this.mainTimeLine.to(".main-page-button", {rotate: 360, scaleX: 0, scaleY: 0, duration: 0.5, })
+    .to(".text", {opacity: 0, duration: 1})
+    .to(".welcome-page", {duration:1, scaleX: 0, scaleY: 0,})
   }
 
   starterAnimation() {
-    this.mainTimeLine.to(".text", {text: "Hi! I'm Marcel!", repeat:1, yoyo: true, duration: 1.5, delay:1.5, repeatDelay: 1,})
-    .to(".text", {text: "Good to see you on my portfolio page :)", duration: 2, delay:0.5})
-    .to(".main-page-button", {y: -500, ease: "elastic.out(1, 0.3)", duration: 2, delay: 1})
+    this.mainTimeLine.to(".text", {text: "Hi! I'm Marcel!",  duration: 1, delay:1, repeatDelay: 0.3,})
+    .to(".second-text", {text: "Good to see you here :)", duration: 2, delay:0.3,})
+    .to(".text", {opacity: 0, duration:0.6, delay:0.5})
+    .to(".second-text", {opacity:0, duration:0.6, delay:0.5})
+    .to(".text", {marginLeft: "50px", marginTop: "150px", text: "This CV-page contains informations about me: <br>", marginRight: 800})
+    .to(".text", { opacity:1, duration: 1.5})
+    .to(".second-text", { marginLeft: "50px", marginTop: "20px", text: " <ul> <li>coding skills </li> <li> coding journey </li> <li> biography </li> <li> and more... </li> </ul> <br>", duration:0.1, marginRight:800})
+    .to(".second-text", {opacity:1, duration:1.5})
+  }
+
+  zlap() {
+    let okno = window.innerWidth
+    return console.log(okno)
   }
 
   ngOnInit(): void {
     this.starterAnimation()
+    this.zlap()
   }
-
 }
