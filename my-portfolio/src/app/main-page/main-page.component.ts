9bb03add385e76580eff9e1d9013899df6a05926
MarcelZapadka <zapadka.m@wp.pl>
2022-10-07 18:24:59 +0200

    Starter page completed

--- my-portfolio/src/app/main-page/main-page.component.ts
+++ my-portfolio/src/app/main-page/main-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
@@ -7,8 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainPageComponent implements OnInit {
 
-  constructor() { }
-
+  
   ngOnInit(): void {
   }
 
--- my-portfolio/src/app/welcome-page/welcome-page.component.ts
+++ my-portfolio/src/app/welcome-page/welcome-page.component.ts
@@ -10,25 +10,36 @@ gsap.registerPlugin(TextPlugin);
 })
 
 export class WelcomePageComponent implements OnInit {
-  
   mainTimeLine = gsap.timeline();
   
-  goToMainPage() {
-    this.mainTimeLine.to(".main-page-button", {rotate: 360, scaleX: 0, scaleY: 0, duration: 0.5, })
-    .to(".text", {opacity: 0, duration: 1})
-    .to(".welcome-page", {duration:1, scaleX: 0, scaleY: 0,})
-  }
-
   starterAnimation() {
     this.mainTimeLine.to(".text", {text: "Hi! I'm Marcel!",  duration: 1, delay:1, repeatDelay: 0.3,})
     .to(".second-text", {text: "Good to see you here :)", duration: 2, delay:0.3,})
     .to(".text", {opacity: 0, duration:0.6, delay:0.5})
     .to(".second-text", {opacity:0, duration:0.6, delay:0.5})
-    .to(".text", {marginLeft: "50px", marginTop: "150px", text: "This CV-page contains informations about me: <br>", marginRight: 800})
-    .to(".text", { opacity:1, duration: 1.5})
-    .to(".second-text", { marginLeft: "50px", marginTop: "20px", text: " <ul> <li>coding skills </li> <li> coding journey </li> <li> biography </li> <li> and more... </li> </ul> <br>", duration:0.1, marginRight:800})
-    .to(".second-text", {opacity:1, duration:1.5})
+    .to(".text", {marginLeft: "50px", marginTop: "150px", marginRight: 800, text: "", fontSize: "33px"})
+    .to(".text", { opacity:1})
+    .to(".text", {text: "This CV page contains this infromation about me <br>", duration:3})
+    .to(".second-text", { marginLeft: "50px", marginTop: "40px" , duration:0.1, marginRight:800, textAlign: "center", text:"", fontSize: "33px"})
+    .to(".second-text", {opacity:1, duration:0.1})
+    .to(".second-text", {text: "coding skills <br> coding journey <br> biography <br>my projects <br> and more...", duration:4})
+    .from(".go-to-main-button", {opacity:0, delay:1, duration:1})
+    .to(".info", {text: "* Press ENTER to skip this page ", duration: 1,})
   }
+
+  pressKeyToSkipAnimations(){
+    window.addEventListener("keydown", (event) => {
+      if(event.key === "Enter") {
+        this.goToMainPage()
+      }
+    })
+  }
+ 
+  goToMainPage() {
+    this.mainTimeLine.kill();
+        gsap.to(".image", {scaleX:8, scaleY:8, duration:2, x: -1800})
+        gsap.to(".image", {y: -100000, delay:1.5})
+  }      
 
   zlap() {
     let okno = window.innerWidth
@@ -38,5 +49,6 @@ export class WelcomePageComponent implements OnInit {
   ngOnInit(): void {
     this.starterAnimation()
     this.zlap()
+    this.pressKeyToSkipAnimations()
   }
 }
